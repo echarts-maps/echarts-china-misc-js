e18ed7b871d0571d75c1a8c6165fe3c7794b2379
chfw <wangc_2011@hotmail.com>
2018-03-05 07:58:55 +0000

    :tada: first version of china misc repository

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+
+// all gulp tasks are located in the ./build/tasks directory
+// gulp configuration is in files in ./build directory
+require('require-dir')('build/tasks');
+
+const TASKS = [
+  '苏皖浙',
+  '华南'
+];
+
+gulp.task("default", TASKS);
