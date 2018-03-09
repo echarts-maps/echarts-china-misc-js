26483d01096477f18431731efab3dd734f0b5373
chfw <wangc_2011@hotmail.com>
2018-03-09 22:49:31 +0000

    :gem: release 0.0.1

--- echarts-china-misc-js/index.js
+++ echarts-china-misc-js/index.js
@@ -1,7 +1,7 @@
 define(["require", "exports"], function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    var version = '1.0.0';
+    var version = '0.0.1';
     function load_ipython_extension() {
         console.log("echarts-china-misc-js " + version + " has been loaded");
     }
